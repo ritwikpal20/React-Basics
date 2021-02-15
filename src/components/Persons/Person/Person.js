f109993b837d36496bf84b874d9dc7ea7c9a64b0
Ritwik Pal <ritwikpal20@gmail.com>
2021-02-15 13:54:24 +0530

    converted to class based component for demonstration of component lifecycle-update

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,23 +1,30 @@
 import classes from "./Person.module.css";
+import { Component } from "react";
 
-const person = (props) => {
-    console.log("Person.js Rendering...");
-    // Creating custom error
-    const rnd = Math.random();
-    if (rnd > 0.9) {
-        throw new Error("Something went wrong. Try again after sometime");
+class Person extends Component {
+    render() {
+        console.log("Person.js Rendering...");
+        // Creating custom error
+        const rnd = Math.random();
+        if (rnd > 0.9) {
+            throw new Error("Something went wrong. Try again after sometime");
+        }
+
+        return (
+            <div className={classes.Person}>
+                {/* deleting the person on clicking first line */}
+                <p onClick={this.props.click}>
+                    I am a {this.props.name} and i am {this.props.age} yrs old
+                    {this.props.children}
+                </p>
+                <input
+                    type="text"
+                    onChange={this.props.change}
+                    value={this.props.name}
+                />
+            </div>
+        );
     }
+}
 
-    return (
-        <div className={classes.Person}>
-            {/* deleting the person on clicking first line */}
-            <p onClick={props.click}>
-                I am a {props.name} and i am {props.age} yrs old
-                {props.children}
-            </p>
-            <input type="text" onChange={props.change} value={props.name} />
-        </div>
-    );
-};
-
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,24 +1,49 @@
 import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
 import Person from "./Person/Person";
+import { Component } from "react";
 
-const persons = (props) => {
-    console.log('Persons.js Rendering...')
-    return (
-        <div>
-            {props.persons.map((person, index) => {
-                return (
-                    <ErrorBoundary key={person.id}>
-                        <Person
-                            name={person.name}
-                            age={person.age}
-                            change={(event) => props.changed(event, index)}
-                            click={() => props.clicked(index)}
-                        />
-                    </ErrorBoundary>
-                );
-            })}
-        </div>
-    );
-};
+class Persons extends Component {
+    static getDerivedStateFromProps(props, state) {
+        console.log("Persons.js getDerivedStateFromProps ", props);
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        // should return true or false depending on whether component should update or not
+        console.log("Persons.js shouldComponentUpdate ", nextProps);
+        return true;
+    }
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log("Persons.js getSnapshotBeforeUpdate ", prevProps);
+        return { message: "brief snapshot" };
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log(
+            "Persons.js componentDidUpdate ",
+            prevProps,
+            " Here are the snapshots: ",
+            snapshot
+        );
+    }
+    render() {
+        console.log("Persons.js Rendering...");
+        return (
+            <div>
+                {this.props.persons.map((person, index) => {
+                    return (
+                        <ErrorBoundary key={person.id}>
+                            <Person
+                                name={person.name}
+                                age={person.age}
+                                change={(event) =>
+                                    this.props.changed(event, index)
+                                }
+                                click={() => this.props.clicked(index)}
+                            />
+                        </ErrorBoundary>
+                    );
+                })}
+            </div>
+        );
+    }
+}
 
-export default persons;
+export default Persons;
